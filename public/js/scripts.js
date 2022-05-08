733108453eb89bb1d7b15ce168773bf6725434f6
Liam Skinner <liamdavidskinner@gmail.com>
2022-05-08 23:21:47 +0100

    chore: installed JS linter: ESLint
    - Installed ESLint v8.15.0
    - Added list of rules
    - Formatted JS code to said rules

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,23 @@
-'use-strict'
+'use-strict';
 
-const nav = document.querySelector('.nav');
-const lines = document.querySelector('.toggle-btn');
+const nav = this.document.querySelector('.nav');
+const lines = this.document.querySelector('.toggle-btn');
 
-document.querySelector('.toggle-btn').addEventListener('click', (event) => {
+this.document.querySelector('.toggle-btn').addEventListener('click', (event) => {
 	nav.classList.contains('show-nav') ? nav.classList.remove('show-nav') : nav.classList.add('show-nav');
 	event.stopPropagation();
 
 	lines.classList.contains('toggle-close') ? lines.classList.remove('toggle-close') : lines.classList.add('toggle-close');
 });
 
-document.addEventListener('click', () => {
+this.document.addEventListener('click', () => {
 	nav.classList.contains('show-nav') && nav.classList.remove('show-nav');
-	lines.classList.contains('toggle-close')  && lines.classList.remove('toggle-close');
+	lines.classList.contains('toggle-close') && lines.classList.remove('toggle-close');
 });
 
-const linesPosition = document.querySelector('.toggle-btn').getBoundingClientRect().top + window.scrollY;
-window.addEventListener('scroll', () => {
-	const WindowPosition = document.querySelector('html').scrollTop;
+const linesPosition = this.document.querySelector('.toggle-btn').getBoundingClientRect().top + this.window.scrollY;
+this.window.addEventListener('scroll', () => {
+	const WindowPosition = this.document.querySelector('html').scrollTop;
 
 	WindowPosition >= linesPosition ?
 		(lines.classList.add('fixed'), nav.classList.add('stickyNav')) :
